681503ead3d00b2f9c617213422dd985ce3683e6
rithgan <rithikgandhi1989@gmail.com>
2022-10-12 14:53:04 +0530

    new pool added

--- src/address.js
+++ src/address.js
@@ -11,3 +11,4 @@ export const LkdPool24 = "0x68b7ee70ab8c692605f0f2345008398c0ec1a20b" //Linkdao 
 export const LkdPool18 = "0xd4769eb0cca4d29af46361052b55767e016df956" //Linkdao pool mainnet
 export const LkdToken = "0xaF027427DC6d31A3e7e162A710a5Fe27e63E275F" //linkdao token mainnet
 export const LkdPoolS24 = "0xed9a44c2a15Cda8f71AaBef6d11D10F377cB7200" //Linkdao pool mainnet
+export const LkdPoolP24 = "0x076243CcA7648d3c93c72EE0caA1A373f478c85B" //Linkdao pool mainnet
--- src/data/pools.js
+++ src/data/pools.js
@@ -1,6 +1,7 @@
 import LKD from '../images/LKD.svg'
-import {LkdPool24,LkdPool18,LkdPoolS24} from "../address"
-import {LkdPool24ABI,LkdPool18ABI,LkdPoolS24ABI} from "../abi"
+import {LkdPool24,LkdPool18,LkdPoolS24,LkdPoolP24} from "../address"
+import {LkdPool24ABI,LkdPool18ABI,LkdPoolS24ABI,LkdPoolP24ABI} from "../abi"
+
 export const data = [
     {
         token:"Recurring Pool A",
@@ -43,5 +44,19 @@ export const data = [
         showAuditLog:false,
         depositOn:false,
         address:"0xed9a44c2a15cda8f71aabef6d11d10f377cb7200"
+    },
+    {
+        token:"Recurring Pool P24",
+        apr:"36.00%",
+        tvl:"-",
+        img:LKD,
+        pool:LkdPoolP24,
+        poolAbi:LkdPoolP24ABI,
+        contract:"https://bscscan.com/address/0x076243cca7648d3c93c72ee0caa1a373f478c85b#code",
+        month:"24",
+        limit:"300000",
+        showAuditLog:false,
+        depositOn:false,
+        address:"0x076243CcA7648d3c93c72EE0caA1A373f478c85B"
     }
 ]
